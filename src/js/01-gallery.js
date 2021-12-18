0126f23a1b9ef262782dfd25e86bf419b00cfceb
julgrabar <45499113+julgrabar@users.noreply.github.com>
2021-12-18 12:27:14 +0200

    Задание 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,26 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const refs = {
+    galleryContainer: document.querySelector('.gallery'),
+    galleryMarkup: createGalleryEl(galleryItems)
+}
+
+ 
+function createGalleryEl (galleryItems){
+    return galleryItems.map(({preview, original, description}) =>{
+        return `
+        
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        
+        `
+    })
+    .join('')
+}
+refs.galleryContainer.insertAdjacentHTML('beforeEnd' , refs.galleryMarkup);
+
+new SimpleLightbox('.gallery a' , {captionDelay: 250 , captionsData: 'alt', captionPosition: 'bottom'});

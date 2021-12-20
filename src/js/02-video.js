47b3eed232bb1f7c4b2f10ccc5aa25f6aaef941e
julgrabar <45499113+julgrabar@users.noreply.github.com>
2021-12-20 18:37:07 +0200

    решение

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+import throttle from "lodash/throttle";
 
+const onPlay = function(data) {
+    let time = data.seconds;
+    localStorage.setItem("videoplayer-current-time", time)
+};
+
+player.on('timeupdate', throttle(onPlay , 1000));
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+         case'RangeError':
+ // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+ // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from "lodash/throttle";
+const form = document.querySelector('.feedback-form');
+ let dataForm = {"email":"","message":""};
+function IsStorageEmpty() {
+    if (localStorage.getItem("feedback-form-state") !== null){
+    let dataFormObj = JSON.parse(localStorage.getItem("feedback-form-state"));
+    form.email.value = dataFormObj.email;
+    form.message.value = dataFormObj.message;
+    dataForm ={"email": dataFormObj.email,'message': dataFormObj.email}
+    };
+};
 
+IsStorageEmpty();
+  
+form.addEventListener('input', throttle((evt)=>{
+    if (evt.target === form.email){
+        dataForm.email = evt.target.value;
+    }else if(evt.target === form.message){
+        dataForm.message = evt.target.value;
+    };
+    localStorage.setItem("feedback-form-state", JSON.stringify(dataForm))
+}, 500));
+
+form.addEventListener('submit', onSubmit)
+function onSubmit(evt) {
+    evt.preventDefault();
+    evt.target.reset();
+    localStorage.removeItem('feedback-form-state');
+    console.log(dataForm);
+    dataForm = {"email":"","message":""}
+    return dataForm;
+}
